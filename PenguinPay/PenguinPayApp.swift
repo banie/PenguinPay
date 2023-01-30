a27625797ea444235ad1fc46807117043be8bfda
Banie Setijoso <banie.setijoso@gmail.com>
2023-01-30 11:03:36 -0500

    Initial Commit

---
+++ PenguinPay/PenguinPayApp.swift
@@ -0,0 +1,17 @@
+//
+//  PenguinPayApp.swift
+//  PenguinPay
+//
+//  Created by Banie Setijoso on 2023-01-30.
+//
+
+import SwiftUI
+
+@main
+struct PenguinPayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
